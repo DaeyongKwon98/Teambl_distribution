7524f3125c0ec192b12878130c6c87c3abacc68f
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-12 10:47:02 +0900

    settings.py에 INSTALLED_APPS api -> backend.api로 수정

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "api",
+    "backend.api",
     "rest_framework",
     "corsheaders",
 ]
