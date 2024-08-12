3e35691b370f02e4a3237b37d8f69a378f153bd7
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-12 11:52:11 +0900

    settings.py database sqlite -> postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,10 +97,17 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": BASE_DIR / "db.postgresql",
     }
 }
 
