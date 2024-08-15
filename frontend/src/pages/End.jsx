75aab28747b9ae1bc8c825d0f74927b8f2ab620f
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-15 22:11:33 +0900

    Update End.jsx

--- frontend/src/pages/End.jsx
+++ frontend/src/pages/End.jsx
@@ -1,33 +1,54 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import '../styles/End.css';
+import { useLocation, useNavigate } from "react-router-dom";
+import "../styles/End.css";
 
-function End(){
+function End() {
   const location = useLocation();
   const navigate = useNavigate();
-  const name ={...location.state}.user_name;
+  const name = { ...location.state }.user_name;
 
-  function handleProfile(){
-    navigate('/')
+  function handleProfile() {
+    localStorage.removeItem("invited"); // 초대받은 경우에만 초대 상태 초기화
+    localStorage.removeItem("invite_code"); // 초대 코드를 삭제
+    navigate("/");
   }
-  function handleLogin(){
-    navigate('/login')
+  function handleLogin() {
+    localStorage.removeItem("invited"); // 초대받은 경우에만 초대 상태 초기화
+    localStorage.removeItem("invite_code"); // 초대 코드를 삭제
+    navigate("/login");
   }
 
-
-
-  return(
-    <div className='end'>
-      <div className='end-container'>
-        <span className='end-name' style={{color: '#0000b3'}}>{name}</span><span className='end-nim'>님,<br/></span>
-        <span  className='end-name'>가입을 축하합니다!</span>
-        <label className='end-label1'>이제 팀블과 함께 최적의 팀원을 탐색해 보세요!</label>
-        <label className='end-label2'>프로필을 더 자세히 작성할수록 다른 회원들과 더 쉽게 연결될 수 있습니다. 이어서 프로필을 작성해 볼까요?</label>
-        <button type='button' className='end-profileBtn' onClick={handleProfile} disabled={true}>프로필 추가로 작성하기</button>
-        <button type='button' className='end-loginBtn' onClick={handleLogin}>팀블 시작하기</button>
-        
+  return (
+    <div className="end">
+      <div className="end-container">
+        <span className="end-name" style={{ color: "#0000b3" }}>
+          {name}
+        </span>
+        <span className="end-nim">
+          님,
+          <br />
+        </span>
+        <span className="end-name">가입을 축하합니다!</span>
+        <label className="end-label1">
+          이제 팀블과 함께 최적의 팀원을 탐색해 보세요!
+        </label>
+        <label className="end-label2">
+          프로필을 더 자세히 작성할수록 다른 회원들과 더 쉽게 연결될 수
+          있습니다. 이어서 프로필을 작성해 볼까요?
+        </label>
+        <button
+          type="button"
+          className="end-profileBtn"
+          onClick={handleProfile}
+          disabled={true}
+        >
+          프로필 추가로 작성하기
+        </button>
+        <button type="button" className="end-loginBtn" onClick={handleLogin}>
+          팀블 시작하기
+        </button>
       </div>
     </div>
   );
 }
 
-export default End
+export default End;
