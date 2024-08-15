64a97abf2541019e84130b43518d3c376a12d280
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-15 22:10:21 +0900

    /pages/NewSearchPage 추가
    pages에 NewSearchPage 폴더와 NewSearch.jsx, MajorPopUp.jsx 추가

---
+++ frontend/src/pages/NewSearchPage/NewSearch.jsx
@@ -0,0 +1,296 @@
+import React, { useState, useEffect } from "react";
+import "../../styles/NewSearch.css";
+import BackIcon from "../../assets/NewSearch/backIcon.svg";
+import RecentIcon from "../../assets/NewSearch/recentIcon.svg";
+import SearchIcon from "../../assets/NewSearch/searchIcon.svg";
+import MajorIcon from "../../assets/NewSearch/majorIcon.svg";
+import NoMajorIcon from "../../assets/NewSearch/nomajorIcon.svg";
+import api from "../../api";
+import NewUserSearchItem from "../../components/NewUserSearchItem";
+import MajorPopUp from "./MajorPopUp";
+
+function NewSearch() {
+  const [users, setUsers] = useState([]); // 검색 결과로 필터링된 유저들
+  const [searchTerm, setSearchTerm] = useState(""); // 검색어
+  const [recentSearches, setRecentSearches] = useState([]); // 최근 검색 기록
+  const [keywords, setKeywords] = useState([]); // DB에서 가져온 키워드들
+  const [searchSuggestions, setSearchSuggestions] = useState([]); // 검색시 아래에 뜨는 추천 검색어
+  const [filters, setFilters] = useState({ relationshipDegree: [], major: [] }); // 사용자가 선택한 검색 필터
+  const [isSearched, setIsSearched] = useState(false); // 사용자가 검색 버튼을 눌렀는지 여부 (true이면 검색 결과창이 보임)
+  const [isMajorPopupOpen, setIsMajorPopupOpen] = useState(false); // major popup이 보이는지 여부
+
+  useEffect(() => {
+    console.log("키워드 가져오기");
+    fetchKeywords(); // 키워드 데이터를 불러오는 함수 호출
+  }, []); // 빈 배열을 의존성으로 전달하여 컴포넌트 마운트 시 한 번만 실행
+
+  useEffect(() => {
+    console.log("필터 변경으로 유저 검색하기");
+    doSearchUsers(); // filters가 변경될 때 실행되는 함수
+  }, [filters]); // filters 배열을 의존성으로 전달하여 filters가 변경될 때마다 실행
+
+  useEffect(() => {
+    if (isSearched) {
+      console.log("검색어 변경으로 유저 검색하기");
+      doSearchUsers();
+    }
+  }, [searchTerm]);
+
+  // DB에서 키워드 목록을 가져오는 함수
+  const fetchKeywords = async () => {
+    try {
+      const response = await api.get("/api/keywords/"); // 키워드 리스트를 가져오는 API 엔드포인트
+      const keywordStrings = response.data.map((item) => item.keyword);
+      setKeywords(keywordStrings); // 키워드 데이터를 keywords 변수에 저장
+      console.log(keywordStrings);
+    } catch (error) {
+      console.error("Failed to fetch keywords:", error);
+    }
+  };
+
+  // 검색을 실행하는 함수
+  const doSearchUsers = async () => {
+    setUsers([]);
+    setIsSearched(true);
+
+    // 최근 검색 기록에 검색어 추가
+    if (searchTerm && !recentSearches.includes(searchTerm)) {
+      setRecentSearches([searchTerm, ...recentSearches.slice(0, 4)]);
+    }
+
+    try {
+      console.log("Search Term:", searchTerm);
+      console.log("Relationship Degree Filters:", filters.relationshipDegree);
+      console.log("Major Filters:", filters.major);
+
+      const response = await api.post("/api/search/", {
+        q: searchTerm,
+        degree: filters.relationshipDegree,
+        major: filters.major,
+      });
+
+      console.log(response.data);
+
+      setUsers(response.data);
+    } catch (error) {
+      console.error("검색 중 오류가 발생했습니다.", error);
+    }
+  };
+
+  // 검색어 변경 시 호출되는 함수
+  const updateSearchSuggestions = (term) => {
+    setSearchTerm(term);
+    setIsSearched(false);
+    if (term) {
+      // 공백 제외 + 소문자로 변환하여 검색
+      setSearchSuggestions(
+        keywords.filter((item) =>
+          item
+            .replace(/\s+/g, "")
+            .toLowerCase()
+            .includes(term.replace(/\s+/g, "").toLowerCase())
+        )
+      );
+    } else {
+      setSearchSuggestions([]);
+    }
+  };
+
+  // 최근 검색어를 지우는 함수
+  const handleClear = () => {
+    setRecentSearches([]);
+  };
+
+  // 검색어를 선택했을 때 호출되는 함수
+  const handleSelect = (term) => {
+    setIsSearched(true);
+    setSearchTerm(term);
+  };
+
+  // 최근 검색어에서 특정 검색어를 삭제하는 함수
+  const handleDelete = (termToDelete) => {
+    setRecentSearches(recentSearches.filter((term) => term !== termToDelete));
+  };
+
+  // Enter 키를 눌렀을 때 검색을 실행하는 함수
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      doSearchUsers();
+    }
+  };
+
+  // n촌 필터를 추가 및 제거하는 함수
+  const handleRelationshipDegreeChange = (degree) => {
+    setFilters((prev) => {
+      const newRelationshipDegree = prev.relationshipDegree.includes(degree)
+        ? prev.relationshipDegree.filter((item) => item !== degree)
+        : [...prev.relationshipDegree, degree];
+      return { ...prev, relationshipDegree: newRelationshipDegree };
+    });
+  };
+
+  // 전공 필터를 추가 및 제거하는 함수
+  const handleMajorChange = (major) => {
+    setFilters((prev) => {
+      const newMajor = prev.major.includes(major)
+        ? prev.major.filter((item) => item !== major)
+        : [...prev.major, major];
+      return { ...prev, major: newMajor };
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="top-bar">
+        <button className="back-button" onClick={() => window.history.back()}>
+          <img src={BackIcon} alt="뒤로가기 아이콘" className="back-icon" />
+        </button>
+        <input
+          type="text"
+          placeholder="팀원을 찾아보세요!"
+          value={searchTerm}
+          onChange={(e) => updateSearchSuggestions(e.target.value)}
+          onKeyPress={handleKeyPress}
+          onClick={() => {
+            setIsSearched(false);
+          }}
+          className="search-input"
+        />
+      </div>
+      {searchTerm && !isSearched ? (
+        <div className="search-results">
+          <ul>
+            {searchSuggestions.map((result, index) => (
+              <li key={index} onClick={() => handleSelect(result)}>
+                <div className="list-item-left">
+                  <span role="img" aria-label="magnifying-glass">
+                    <img src={SearchIcon} alt="검색" className="search-icon" />
+                  </span>
+                  <span
+                    dangerouslySetInnerHTML={{
+                      __html: result.replace(
+                        new RegExp(searchTerm, "gi"),
+                        (match) => `<span class="highlight">${match}</span>`
+                      ),
+                    }}
+                  />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : !isSearched ? (
+        <>
+          <div className="second-bar">
+            <h3>최근 검색어</h3>
+            <button className="clear-button" onClick={handleClear}>
+              지우기
+            </button>
+          </div>
+          <div className="search-results">
+            <ul>
+              {recentSearches.map((term, index) => (
+                <li key={index} onClick={() => handleSelect(term)}>
+                  <div className="list-item-left">
+                    <span role="img" aria-label="clock">
+                      <img
+                        src={RecentIcon}
+                        alt="최근검색 아이콘"
+                        className="recent-icon"
+                      />
+                    </span>
+                    {term}
+                  </div>
+                  <button
+                    className="delete-button"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleDelete(term);
+                    }}
+                  >
+                    삭제
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="filter-bar">
+            <button
+              className={`filter-button ${
+                filters.relationshipDegree.includes(1) ? "active" : ""
+              }`}
+              onClick={() => {
+                handleRelationshipDegreeChange(1);
+              }}
+            >
+              1촌
+            </button>
+            <button
+              className={`filter-button ${
+                filters.relationshipDegree.includes(2) ? "active" : ""
+              }`}
+              onClick={() => {
+                handleRelationshipDegreeChange(2);
+              }}
+            >
+              2촌
+            </button>
+            <button
+              className={`filter-button ${
+                filters.relationshipDegree.includes(3) ? "active" : ""
+              }`}
+              onClick={() => {
+                handleRelationshipDegreeChange(3);
+              }}
+            >
+              3촌
+            </button>
+            <button
+              className={`filter-button ${
+                isMajorPopupOpen || filters.major.length > 0 ? "active" : ""
+              }`}
+              onClick={() => setIsMajorPopupOpen(true)}
+            >
+              {filters.major.length > 0
+                ? filters.major.length > 1
+                  ? `전공 ${filters.major.length} `
+                  : filters.major[0] + " "
+                : "전공 "}
+              {filters.major.length > 0 || isMajorPopupOpen ? (
+                <img src={MajorIcon} alt="전공 아이콘" className="major-icon" />
+              ) : (
+                <img
+                  src={NoMajorIcon}
+                  alt="0전공 아이콘"
+                  className="nomajor-icon"
+                />
+              )}
+            </button>
+          </div>
+          <div className="team-member-results">
+            {users.map((user, index) => (
+              <NewUserSearchItem
+                user={user}
+                onAddRelationShip={() => {}}
+                key={index}
+              />
+            ))}
+          </div>
+        </>
+      )}
+      {isMajorPopupOpen && (
+        <MajorPopUp
+          filters={filters}
+          handleMajorChange={handleMajorChange}
+          setIsMajorPopupOpen={setIsMajorPopupOpen}
+          doSearchUsers={doSearchUsers}
+        />
+      )}
+    </div>
+  );
+}
+
+export default NewSearch;
