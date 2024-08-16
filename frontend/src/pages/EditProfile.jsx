6b73a8f62b1d9aed248d6dbbf1b68c12c6b92242
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-16 17:39:24 +0900

    Create EditProfile.jsx

---
+++ frontend/src/pages/EditProfile.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate , useLocation} from "react-router-dom";
+import '../styles/EditProfile.css'
+
+function EditProfile() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const userInfo = {...location.state};
+
+  const [savedUser_name, setSavedUserName] = useState(userInfo.user_name);
+  const [savedSchool, setSavedSchool] = useState(userInfo.school);
+  const [savedCurrent_academic_degree, setSavedCurrentAcademicDegree] = useState(userInfo.current_academic_degree);
+  const [savedYear, setSavedYear] = useState(userInfo.year);
+  const [savedMajor, setSavedMajor] = useState(userInfo.major);
+
+
+  const [user_name, setUserName] = useState(savedUser_name);
+  const [school, setSchool] = useState(savedSchool);
+  const [current_academic_degree, setCurrentAcademicDegree] = useState(savedCurrent_academic_degree);
+  const [year, setYear] = useState(savedYear);
+  const [major, setMajor] = useState(savedMajor);
+
+  function handleBack(){
+    navigate('/profile', {
+      state: {
+        user_name : savedUser_name,
+        school : savedSchool,
+        current_academic_degree : savedCurrent_academic_degree,
+        year : savedYear,
+        major : savedMajor,
+        prevPage : 'editprofile'
+      }
+    });
+  }
+
+  function handleDegree(){
+    var degreeList = document.getElementById('list');
+    var value = (degreeList.options[degreeList.selectedIndex].text);
+    setCurrentAcademicDegree(value);
+  }
+
+  function handleSave(){
+    setSavedUserName(user_name)
+    setSavedSchool(school)
+    setSavedCurrentAcademicDegree(current_academic_degree)
+    setSavedYear(year)
+    setSavedMajor(major)
+    // navigate('/profile', {
+    //   state: {
+    //     user_name : savedUser_name,
+    //     school : savedSchool,
+    //     current_academic_degree : savedCurrent_academic_degree,
+    //     year : savedYear,
+    //     major : savedMajor,
+    //     prevPage : 'editprofile'
+    //   }
+    // });
+  }
+
+  return (
+    <form className='edit' >  
+      <div className='edit-back'>
+        <button type="button" onClick={handleBack}></button>
+      </div>
+      <h4>프로필 작성하기</h4>
+      <div className="edit-container">
+        <label className='edit-label'>이름<br/></label>
+        <input 
+          type='text' 
+          placeholder=' 이름 입력' 
+          className='edit-input'
+          onChange={(e)=>(setUserName(e.target.value))} 
+          value={user_name}
+          required
+        />
+        <label className='edit-label'>학교<br/></label>
+        <input 
+          type='text' 
+          placeholder=' 학교 입력' 
+          className='edit-input' 
+          onChange={(e)=>(setSchool(e.target.value))} 
+          value={school}
+          required
+        />
+        <label className='edit-label'>학력<br/></label>
+        <select onChange={handleDegree} id='list' defaultValue={savedCurrent_academic_degree}>
+          <option value='학사'>학사</option>
+          <option value='석사'>석사</option>
+          <option value='박사'>박사</option>
+        </select >
+        <label className='edit-label'>입학년도<br/></label>
+        <input 
+          type='number' 
+          placeholder=' 입학년도 입력(4자리)' 
+          className='edit-input' 
+          onChange={(e)=>(setYear(e.target.value))} 
+          value={year}
+          required
+          min='1900'
+          max={new Date().getFullYear()} // 현재 연도까지 입력 가능
+        />
+        <label className='edit-label'>전공<br/></label>
+        <input 
+          type='search' 
+          placeholder='전공 입력' 
+          className='edit-input' 
+          onChange={(e)=>(setMajor(e.target.value))} 
+          value={major}
+          required
+        />
+        <button 
+          type='button' 
+          className='edit-nextBtn'
+          onClick={handleSave}
+        >저장</button>
+      </div>
+    </form>
+  );
+}
+
+export default EditProfile;
