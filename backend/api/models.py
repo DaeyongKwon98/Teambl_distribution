c12f7615edecdaee31c68ebc2d4069381ba17219
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-20 17:32:10 +0900

    Update models.py
    Notification 추가

--- backend/api/models.py
+++ backend/api/models.py
@@ -231,3 +231,15 @@ class Friend(models.Model):
 
     def __str__(self):
         return f"{self.from_user.email} is friends with {self.to_user.email}, status: {self.status}"
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="notifications"
+    )
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Notification for {self.user.username} - {self.message}"
