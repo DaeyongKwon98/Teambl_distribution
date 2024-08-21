5ad5de614e7c6c91290c43eb1240a8c6bd1cb0d7
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-21 23:24:30 +0900

    Update models.py
    Noficiation에 notification_type 추가

--- backend/api/models.py
+++ backend/api/models.py
@@ -234,12 +234,22 @@ class Friend(models.Model):
 
 
 class Notification(models.Model):
+    NOTIFICATION_TYPE_CHOICES = [
+        ('invitation_register', 'Invitation Register'),
+        ('friend_accept', 'Friend Accept'),
+        ('friend_reject', 'Friend Reject'),
+        ('friend_request', 'Friend Request'),
+    ]
+
     user = models.ForeignKey(
         CustomUser, on_delete=models.CASCADE, related_name="notifications"
     )
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
+    notification_type = models.CharField(
+        max_length=20, choices=NOTIFICATION_TYPE_CHOICES
+    )
 
     def __str__(self):
         return f"Notification for {self.user.username} - {self.message}"
