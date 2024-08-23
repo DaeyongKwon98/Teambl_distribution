bf4959ddd7d6eb14519b2619d1ebdb510938c614
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-23 22:47:35 +0900

    Update EditProfile.jsx
    import api 추가

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate , useLocation } from "react-router-dom";
 import '../styles/EditProfile.css'
+import api from "../api";
 
 function EditProfile() {
   const navigate = useNavigate();
