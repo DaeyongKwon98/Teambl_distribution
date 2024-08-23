091de41e3eff99104664fccef729923beb045c54
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-24 03:03:47 +0900

    Update urls.py
    TokenObtainPairView를 MyTokenObtainPairView로 변경

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 #from backend.api.views import CreateUserView
-from api.views import CreateUserView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api.views import CreateUserView, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView#, TokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
-    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/", MyTokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     #path("api/", include("backend.api.urls")), # api/로 시작하는데 위에 속하지 않는경우, 남은 url을 api.urls로 보내라
