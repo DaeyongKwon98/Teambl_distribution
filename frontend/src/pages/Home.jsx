c856dd2c8cf1a057f2aadca3f275dbc2c3f6dbaf
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-25 00:29:21 +0900

    Update Home.jsx
    import useEffect

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
 import "../styles/Home.css";
