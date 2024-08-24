c61035982f8287a7af1608c3020a7fbf683ac749
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-24 23:35:46 +0900

    Update Home.jsx
    비밀번호 변경, 계정 삭제 기능 옮기기 (주석처리)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -49,43 +49,43 @@ function Home() {
     navigate("/setting");
   }
   
-  const handleChangePassword = async () => {
-    try {
-      const response = await api.patch("/api/change-password/", {
-        new_password: newPassword,
-      });
-      setNewPassword(""); // 비밀번호 변경 후 입력 필드 초기화
+  // const handleChangePassword = async () => {
+  //   try {
+  //     const response = await api.patch("/api/change-password/", {
+  //       new_password: newPassword,
+  //     });
+  //     setNewPassword(""); // 비밀번호 변경 후 입력 필드 초기화
 
-      // 로그아웃 처리
-      localStorage.removeItem("token"); // 토큰 제거
-      alert("Password changed successfully. You will be logged out.");
-      navigate("/login"); // 로그인 페이지로 이동
-    } catch (error) {
-      console.error("Password change failed:", error);
-      alert("Password change failed");
-    }
-  };
+  //     // 로그아웃 처리
+  //     localStorage.removeItem("token"); // 토큰 제거
+  //     alert("Password changed successfully. You will be logged out.");
+  //     navigate("/login"); // 로그인 페이지로 이동
+  //   } catch (error) {
+  //     console.error("Password change failed:", error);
+  //     alert("Password change failed");
+  //   }
+  // };
 
-  const handleDeleteAccount = async () => {
-    if (
-      window.confirm(
-        "Are you sure you want to delete your account? This action cannot be undone."
-      )
-    ) {
-      try {
-        const response = await api.delete("/api/delete-user/");
-        if (response && response.data && response.data.detail) {
-          alert(response.data.detail);
-        } else {
-          alert("Account deleted successfully.");
-        }
-        navigate("/login"); // 계정 삭제 후 로그인 페이지로 이동
-      } catch (error) {
-        console.error("Account deletion failed:", error);
-        alert("Account deletion failed");
-      }
-    }
-  };
+  // const handleDeleteAccount = async () => {
+  //   if (
+  //     window.confirm(
+  //       "Are you sure you want to delete your account? This action cannot be undone."
+  //     )
+  //   ) {
+  //     try {
+  //       const response = await api.delete("/api/delete-user/");
+  //       if (response && response.data && response.data.detail) {
+  //         alert(response.data.detail);
+  //       } else {
+  //         alert("Account deleted successfully.");
+  //       }
+  //       navigate("/login"); // 계정 삭제 후 로그인 페이지로 이동
+  //     } catch (error) {
+  //       console.error("Account deletion failed:", error);
+  //       alert("Account deletion failed");
+  //     }
+  //   }
+  // };
 
 
 //규원's 뉴 홈페이지
@@ -214,9 +214,7 @@ function Home() {
           </div>
         </div>
       )}
-
-
-
+      
       <h1>Home Page</h1>
       <button onClick={goToProjects} className="button1">
         프로젝트
@@ -236,7 +234,7 @@ function Home() {
       <button onClick={goToNotification} className="button6">
         알림
       </button>
-      <div className="password-change-container">
+      {/* <div className="password-change-container">
         <h2>Change Password</h2>
         <input
           type="password"
@@ -250,7 +248,7 @@ function Home() {
         <h2>Delete Account</h2>
         <button onClick={handleDeleteAccount}>Delete Account</button>
       </div>
-    </div>
+    </div> */}
   );
 }
 
