d3f7c6d16cdc3fdce2040e0b4b5a6a6f1feeb319
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-24 23:36:24 +0900

    Create Setting.jsx

---
+++ frontend/src/pages/Setting.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Setting.css';
+import api from "../api";
+
+const Setting = () => {
+  const navigate = useNavigate();
+  const [newPassword, setNewPassword] = useState("");
+
+  const handleChangePassword = async () => {
+    try {
+      const response = await api.patch("/api/change-password/", {
+        new_password: newPassword,
+      });
+      setNewPassword(""); // 비밀번호 변경 후 입력 필드 초기화
+
+      // 로그아웃 처리
+      localStorage.removeItem("token"); // 토큰 제거
+      alert("Password changed successfully. You will be logged out.");
+      navigate("/login"); // 로그인 페이지로 이동
+    } catch (error) {
+      console.error("Password change failed:", error);
+      alert("Password change failed");
+    }
+  };
+
+  const handleDeleteAccount = async () => {
+    if (
+      window.confirm(
+        "Are you sure you want to delete your account? This action cannot be undone."
+      )
+    ) {
+      try {
+        const response = await api.delete("/api/delete-user/");
+        if (response && response.data && response.data.detail) {
+          alert(response.data.detail);
+        } else {
+          alert("Account deleted successfully.");
+        }
+        navigate("/login"); // 계정 삭제 후 로그인 페이지로 이동
+      } catch (error) {
+        console.error("Account deletion failed:", error);
+        alert("Account deletion failed");
+      }
+    }
+  };
+
+  return (
+    <div className="setting-container">
+        <h2>Change Password</h2>
+        <input
+          type="password"
+          placeholder="New Password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <button onClick={handleChangePassword}>Change Password</button>
+      </div>
+      <div className="account-delete-container">
+        <h2>Delete Account</h2>
+        <button onClick={handleDeleteAccount}>Delete Account</button>
+      </div>
+    </div>
+  );
+};
+
+export default Setting;
