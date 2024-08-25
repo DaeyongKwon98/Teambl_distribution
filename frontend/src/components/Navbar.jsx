557c215c900a6934486d4b90cde5419fb7aee35d
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-25 23:55:26 +0900

    Create Navbar.jsx

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navbar = ({ activeNav, handleNavClick }) => {
+  return (
+    <nav className="home-navbar">
+      {['홈', '1촌', '초대', '설정'].map((item) => (
+        <span
+          key={item}
+          className={`home-nav-item ${activeNav === item ? 'home-active' : ''}`}
+          onClick={() => handleNavClick(item)}
+        >
+          {item}
+        </span>
+      ))}
+    </nav>
+  );
+};
+
+export default Navbar;
