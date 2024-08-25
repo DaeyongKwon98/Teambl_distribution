86e1317310537d20394b6f6b8f083db1333cd947
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-26 01:08:58 +0900

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
