7a50541d53c303177daabdc2bd33da3da84fb5aa
Gyuwon Lee <gyuwon@kaist.ac.kr>
2024-08-27 16:07:13 +0900

    설정 구현

--- frontend/src/pages/Setting.jsx
+++ frontend/src/pages/Setting.jsx
@@ -4,30 +4,80 @@ import '../styles/Setting.css';
 import api from "../api";
 import Header from '../components/Header';
 import Navbar from '../components/Navbar';
+import WithdrawIcon from '../assets/withdrawIcon.svg';
 
 const Setting = () => {
   const navigate = useNavigate();
-  const [newPassword, setNewPassword] = useState("");
   const [activeNav, setActiveNav] = useState("설정");
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+  const [withdrawPassword, setWithdrawPassword] = useState('');
+  const [changePasswordError, setChangePasswordError] = useState('');
+  const [withdrawError, setWithdrawError] = useState('');
+  const [changeBtnActive, setChangeBtnActive] = useState(false);
+  const [withdrawBtnActive, setWithdrawBtnActive] = useState(false);
+  const [showWithdrawModal, setShowWithdrawModal] = useState(false);
+  const [showFinalModal, setShowFinalModal] = useState(false);
+
+  const DbPassword = '1234';
+
+  useEffect(() => {
+    setChangeBtnActive(
+      currentPassword !== '' &&
+      newPassword !== '' &&
+      confirmNewPassword !== ''
+    );
+
+    setWithdrawBtnActive(withdrawPassword !== '');
+  }, [currentPassword, newPassword, confirmNewPassword, withdrawPassword]);
 
   const handleChangePassword = async () => {
-    try {
-      const response = await api.patch("/api/change-password/", {
-        new_password: newPassword,
-      });
-      setNewPassword(""); // 비밀번호 변경 후 입력 필드 초기화
-
-      // 로그아웃 처리
-      localStorage.removeItem("token"); // 토큰 제거
-      alert("Password changed successfully. You will be logged out.");
-      navigate("/login"); // 로그인 페이지로 이동
-    } catch (error) {
-      console.error("Password change failed:", error);
-      alert("Password change failed");
+    if (currentPassword !== DbPassword) {
+      setChangePasswordError('현 비밀번호가 일치하지 않습니다');
+    } else if (newPassword !== confirmNewPassword) {
+      setChangePasswordError('새 비밀번호가 일치하지 않습니다');
+    } else {
+      setChangePasswordError('');
+      try {
+        const response = await api.patch("/api/change-password/", {
+          new_password: newPassword,
+        });
+        setNewPassword(""); // 비밀번호 변경 후 입력 필드 초기화
+  
+        // 로그아웃 처리
+        localStorage.removeItem("token"); // 토큰 제거
+        alert("Password changed successfully. You will be logged out.");
+        navigate("/login"); // 로그인 페이지로 이동
+      } catch (error) {
+        console.error("Password change failed:", error);
+        alert("Password change failed");
+      }
     }
   };
 
-  const handleDeleteAccount = async () => {
+  const handleLogout = () => {
+    // 로그아웃 처리
+    localStorage.removeItem("token"); // 토큰 제거
+    alert("Password changed successfully. You will be logged out.");
+    navigate("/login"); // 로그인 페이지로 이동
+  };
+
+  const handleWithdraw = () => {
+    if (withdrawPassword !== DbPassword) {
+      setWithdrawError('현 비밀번호가 일치하지 않습니다');
+    } else {
+      setShowWithdrawModal(true);
+    }
+  };
+
+  const confirmWithdraw = () => {
+    setShowWithdrawModal(false);
+    setShowFinalModal(true);
+  };
+
+  const handleFinalConfirmation = async () => {
+    setShowFinalModal(false);
     if (
       window.confirm(
         "Are you sure you want to delete your account? This action cannot be undone."
@@ -46,6 +96,10 @@ const Setting = () => {
         alert("Account deletion failed");
       }
     }
+  };
+
+  const closeWithdrawModal = () => {
+    setShowWithdrawModal(false);
   };
 
   const handleNavClick = (item) => {
@@ -70,19 +124,117 @@ const Setting = () => {
       <Header />
       <Navbar activeNav={activeNav} handleNavClick={handleNavClick} />
       
-      <h2>Change Password</h2>
-      <input
-        type="password"
-        placeholder="New Password"
-        value={newPassword}
-        onChange={(e) => setNewPassword(e.target.value)}
-      />
-      <button onClick={handleChangePassword}>Change Password</button>
-    
-      <div className="account-delete-container">
-        <h2>Delete Account</h2>
-        <button onClick={handleDeleteAccount}>Delete Account</button>
+
+      <h2 className="setting-section-title">비밀번호 변경</h2>
+      <div className="setting-password-change-section">
+        <div className="setting-input-group">
+          <label>현 비밀번호</label>
+          <input
+            type="password"
+            value={currentPassword}
+            onChange={(e) => setCurrentPassword(e.target.value)}
+          />
+        </div>
+        <div className="setting-input-group">
+          <label>새 비밀번호</label>
+          <input
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+        </div>
+        <div className="setting-input-group">
+          <label>새 비밀번호 확인</label>
+          <input
+            type="password"
+            value={confirmNewPassword}
+            onChange={(e) => setConfirmNewPassword(e.target.value)}
+          />
+        </div>
+        {changePasswordError && <p className="setting-error">{changePasswordError}</p>}
+        <button
+          className="setting-change-password-button"
+          onClick={handleChangePassword}
+          disabled={!changeBtnActive}
+        >
+          비밀번호 변경
+        </button>
       </div>
+
+      <h2 className="setting-section-title">로그 아웃</h2>
+      <button className="setting-logout-button" onClick={handleLogout}>로그 아웃</button>
+
+      <h2 className="setting-section-title">회원 탈퇴</h2>
+      <div className="setting-withdraw-section">
+        <div className="setting-input-group">
+          <label>현 비밀번호</label>
+          <input
+            type="password"
+            value={withdrawPassword}
+            onChange={(e) => setWithdrawPassword(e.target.value)}
+          />
+        </div>
+        {withdrawError && <p className="setting-error">{withdrawError}</p>}
+        <button
+          className="setting-withdraw-button"
+          disabled={!withdrawBtnActive}
+          onClick={handleWithdraw}
+        >
+          회원 탈퇴
+        </button>
+      </div>
+
+      {showWithdrawModal && (
+        <div className="setting-modal-overlay">
+          <div className="setting-withdraw-modal-content">
+            <div className="setting-modal-title">
+              <img
+                src={WithdrawIcon}
+                alt='탈퇴 아이콘'
+                className='setting-withdraw-icon'
+              />
+              <p>정말 탈퇴하시겠어요?</p>
+            </div>
+            <p className="setting-modal-description">
+              탈퇴 버튼 선택 시, 계정은<br />
+              삭제되며 복구되지 않습니다.
+            </p>
+            <div className="setting-modal-buttons">
+              <button
+                className="setting-modal-button setting-cancel-button"
+                onClick={closeWithdrawModal}
+              >
+                취소
+              </button>
+              <button
+                className="setting-modal-button setting-confirm-button"
+                onClick={confirmWithdraw}
+              >
+                탈퇴
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showFinalModal && (
+        <div className="setting-modal-overlay">
+          <div className="setting-withdraw-modal-content">
+            <p className="setting-final-modal-title">탈퇴 처리가 완료되었습니다.</p>
+            <p className="setting-final-modal-description">
+              이용해 주셔서 감사합니다.<br />
+              앞으로 더 좋은 모습으로 만나뵐 수<br />
+              있도록 노력하겠습니다.
+            </p>
+            <button
+              className="setting-final-confirm-button"
+              onClick={handleFinalConfirmation}
+            >
+              확인
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
