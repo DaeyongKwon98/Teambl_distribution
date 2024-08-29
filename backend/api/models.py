b3e5fb4e94e0a019c0dba7ac7d51e38f9626945f
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-29 17:01:24 +0900

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -356,3 +356,14 @@ class Notification(models.Model):
 
     def __str__(self):
         return f"Notification for {self.user.email} - {self.message}"
+
+
+class Inquiry(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="inquiries"
+    )
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Inquiry from {self.user.username} at {self.created_at}'
