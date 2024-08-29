85fc06e27729dbd44a103bf3590b0e4ee211f3f4
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-08-29 11:42:44 +0900

    Update FriendCard.jsx

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -3,11 +3,21 @@ import BefriIcon from "../assets/befriIcon.svg";
 
 const FriendCard = ({ friend, isKeywordFriend = false }) => {
   console.log("friend", friend);
+  
+  // 1촌 정보를 표시하는 함수
+  const renderFriendOf = () => {
+    if (friend.numFriends === 1) {
+      return `${friend.friendOf}의 1촌`;
+    } else if (friend.numFriends > 1) {
+      return `${friend.friendOf} 외 공통 1촌 ${friend.numFriends-1}명`;
+    }
+  };
+  
   return (
     <div className="home-friend-card">
       <div className='home-same-and-befri'>
         <div className={isKeywordFriend ? "home-same-tag" : "home-friend-of"}>
-          <span>{isKeywordFriend ? `#${friend.sametag}` : `${friend.friendOf}의 1촌`}</span>
+          <span>{isKeywordFriend ? `#${friend.sametag}` : renderFriendOf()}</span>
         </div>
         <img src={BefriIcon} alt="1촌신청" className="home-befri-icon" />
       </div>
