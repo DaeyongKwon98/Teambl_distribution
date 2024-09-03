5402b274b859551d48a9b59b5c9e9b4c4513a484
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-09-03 14:36:24 +0900

    Update ProfileOther.jsx

--- frontend/src/pages/ProfilePage/ProfileOther.jsx
+++ frontend/src/pages/ProfilePage/ProfileOther.jsx
@@ -72,7 +72,7 @@ const ProfileOther = ({ userId }) => {
   return (
     <div className="profileOther-body">
       <div className="profileOther-container">
-        <button className="profileOther-backbutton" onClick={handleBackButton}>
+        <button className="profileOther-backbutton" onClick={window.history.back()}>
           <img src={backIcon}></img>
         </button>
 
