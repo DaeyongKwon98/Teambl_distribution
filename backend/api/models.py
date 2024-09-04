f6dc06be138171db8bfb3f2ed2814ef328a6d685
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-09-04 13:57:51 +0900

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -361,3 +361,14 @@ class Inquiry(models.Model):
 
     def __str__(self):
         return f'Inquiry from {self.user.username} at {self.created_at}'
+
+class SearchHistory(models.Model):
+    user = models.ForeignKey(
+        CustomUser, on_delete=models.CASCADE, related_name="search_histories"
+    )
+    keyword = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Search by {self.user.username} for "{self.keyword}" at {self.created_at}'
+
