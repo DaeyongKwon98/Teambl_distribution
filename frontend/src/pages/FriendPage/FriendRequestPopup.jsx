5b90e5943f537a4ec836e35195f9c226d07e27e1
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-09-19 00:18:40 +0900

    Update FriendRequestPopup.jsx

--- frontend/src/pages/FriendPage/FriendRequestPopup.jsx
+++ frontend/src/pages/FriendPage/FriendRequestPopup.jsx
@@ -1,41 +1,40 @@
 import React from "react";
 import "../../styles/FriendPage/FriendDeletePopup.css"; // 기존 스타일 가져오기
-import topBarIcon from "../../assets/popUpTopBar.svg";
-import blueXIcon from "../../assets/Friend/blue_x_icon.svg";
+import blueXIcon from "../../assets/Friend/blue_x_icon.svg"; // 아이콘 가져오기
 
-const FriendRequestPopup = ({ setIsPopupOpen, handleConfirm }) => {
-  const handleOverlayClick = (e) => {
-    if (e.target === e.currentTarget) {
-      setIsPopupOpen(false); // 팝업 외부 클릭 시 닫기
-    }
-  };
-
+const FriendRequestPopup = ({ profile, closeFriendDeleteModal, addFriend }) => {
   return (
-    <div className="fd-popup-overlay-wrapper" onClick={handleOverlayClick}>
-      <div className="fd-popup-overlay">
-        <div className="fd-popup-top">
-          <img src={topBarIcon} alt="popup top bar" />
+    <div className="fd-modal-overlay">
+      <div className="fd-withdraw-modal-content">
+        <div className="fd-modal-title">
+          <img
+            src={blueXIcon} // 동일한 아이콘 사용
+            alt="탈퇴 아이콘"
+            className="fd-withdraw-icon"
+          />
+          <p>1촌 신청</p>
         </div>
-        <div className="fd-popup-radio-container">
-          <p>
-            1촌은 최대 50명까지만 신청할 수 있습니다.<br />
-            함께하는 신뢰 커뮤니티를 위해 신중하게 결정해 주세요.
-          </p>
-          <div className="fd-popup-buttons">
-            <button
-              className="fd-delete-confirm-button"
-              onClick={handleConfirm} // 최종 확인 팝업으로 이동
-            >
-              <img src={blueXIcon} alt="blue x icon" className="fd-bluex-icon" />
-              1촌 신청
-            </button>
-            <button
-              className="fd-cancel-button"
-              onClick={() => setIsPopupOpen(false)}
-            >
-              취소
-            </button>
-          </div>
+        <p className="fd-modal-description">
+          {profile.user_name}님께 1촌을 신청하시겠습니까?
+          <br />
+          1촌은 최대 50명까지 가능하므로 신중히 결정해주세요.
+        </p>
+        <div className="fd-modal-buttons">
+          <button
+            className="fd-modal-button fd-cancel-button"
+            onClick={closeFriendDeleteModal}
+          >
+            취소
+          </button>
+          <button
+            className="fd-modal-button fd-confirm-button"
+            onClick={() => {
+              addFriend(); // 1촌 신청 함수 호출
+              closeFriendDeleteModal(); // 모든 팝업 닫기
+            }}
+          >
+            신청
+          </button>
         </div>
       </div>
     </div>
