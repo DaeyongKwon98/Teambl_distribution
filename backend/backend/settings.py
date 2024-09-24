d27d80a9067e46ab222ab92bb75936f65388eeba
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-09-25 00:54:00 +0900

    DB postgresql로 변경

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,17 +119,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": 'django.db.backends.mysql',
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME"),
         "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT", "3306"),
-        'OPTIONS':{
-            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
-    
 }
 
 print("DB_NAME:", os.getenv("DB_NAME"))
