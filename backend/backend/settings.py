cdf57dd3d82e080d5573e79e19288bd4906bf8fd
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-09-25 00:49:58 +0900

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,27 +110,27 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": 'django.db.backends.mysql',
-#         "NAME": os.getenv("DB_NAME"),
-#         "USER": os.getenv("DB_USER"),
-#         "PASSWORD": os.getenv("DB_PASSWORD"),
-#         "HOST": os.getenv("DB_HOST"),
-#         "PORT": os.getenv("DB_PORT", "3306"),
-#         'OPTIONS':{
-#             'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
-#         },
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
 #     }
-    
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.mysql',
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+    }
+    
+}
 
 print("DB_NAME:", os.getenv("DB_NAME"))
 print("DB_USER:", os.getenv("DB_USER"))
