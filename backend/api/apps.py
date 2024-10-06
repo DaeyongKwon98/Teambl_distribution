0b3244080900a095cd16322ede52b97447e0d432
DaeyongKwon98 <70640395+DaeyongKwon98@users.noreply.github.com>
2024-10-06 11:00:57 +0900

    project 삭제 시 /media/project_images에서도 사진 삭제

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -3,5 +3,8 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    #name = "backend.api"
     name = "api"
+
+    def ready(self):
+        # Import signals here to ensure they are registered
+        import api.signals
